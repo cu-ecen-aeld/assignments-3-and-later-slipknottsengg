63cf1f0cd8aac0d193a0f20aedb6c53ed11bb5c7
Anna Hui-Ming Tseng <111482486+slipknottsengg@users.noreply.github.com>
2024-12-03 12:52:01 +0000

    assignment-4

--- finder-app/writer.sh
+++ finder-app/writer.sh
@@ -1,49 +1,29 @@
 #!/bin/bash
 
-# arguments
-writefile=$1
-writestr=$2
+function my_error()
+{
+    # maybe we want some logging functionality later?
+    echo "${1}"
+    exit 1
+}
 
-# check that two arguments were input
-if [ $# -ne 2 ]
-then
-        echo "Error! Please provide two arguments: Path and file name to create, text string to write to file"
-        exit 1
+function my_success()
+{
+    if [ ! -z "${1}" ] ; then
+	echo "${1}"
+    fi
+    exit 0
+}
+
+if [ "$#" != "2" ] ; then my_error "Not enough arguments" ; fi
+if [ -d "${1}" ] ; then my_error "${1} is a directory" ; fi
+
+dir=`dirname "${1}"`
+if [ ! -d "${dir}" ] ; then
+    echo "Creating directory \"${dir}\"..."
+    mkdir -p "${dir}"
 fi
+echo "Writing file \""`basename "${1}"`"\"..."
+echo "${2}" > ${1}
 
-# build array to manage paths, replace "/" with "/ " to allow easier concatenation
-array=(${writefile///// })
-
-#echo "Original array: ${array[@]}"
-
-# remove last element (last element is the file name)
-unset 'array[-1]'
-#echo "Array without the file name: ${array[@]}"
-
-# if the first element of the array is just / append that to the first element
-if [ ${array[0]} = "/" ] 
-then
-	array[1]="/${array[1]}"
-	unset 'array[0]'
-	#echo "Array after changes: ${array[@]}"
-fi	
-
-# create all directories needed
-for path_var in ${array[@]}
-do
-	# append path
-        temp_path="$temp_path$path_var"
-        #echo "This is the path to test: $temp_path"
-	if [ ! -d $temp_path ]
-	then
-		#echo "Path does not exist, create folder"
-		mkdir $temp_path
-	#else
-		#echo "Path exists, no need to create folder"
-	fi
-	#echo $path_var
-done
-
-
-echo $writestr > $writefile
-#echo "File $writefile created (or replaced) with content $writestr"
+my_success
